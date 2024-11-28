78276297b6b3c5d3c740f0934a891afe9eb6622b
Pecola <ephialtes11@gmail.com>
2024-11-28 17:28:03 +1100

    completed etch sketch with random color gen completed button to resize canvas size

--- javaFile.js
+++ javaFile.js
@@ -1,7 +1,24 @@
+let squareResolution = 0; 
 
-const resizeButton = document.querySelector("button"); 
+const resizeButton = document.querySelector("#areaSize"); 
 resizeButton.addEventListener("click",()=>{
+let newSize = prompt("Insert any new square resolution below 100")
+if (newSize<=100){
+    const sketchArea = document.querySelector("#sketchArea"); 
+    deleteChild();  
+    draw(newSize); 
+}
+}); 
 
+function deleteChild() {
+    let e = document.querySelector("div");
+
+    //e.firstElementChild can be used. 
+    let child = e.lastElementChild;
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
 }
 
 
@@ -19,7 +36,8 @@ for (let i=1; i<=totalSquares;i++){
     newSquare.style.boxSizing = "border-box"; 
     newSquare.addEventListener("mouseover",(e)=>{
         let target = e.target; 
-        target.style.backgroundColor ="black";
+        randomColor = Math.round((Math.random()*10**6));  
+        target.style.backgroundColor = `#${randomColor}`;
     }); 
     sketchArea.appendChild(newSquare); 
    }
